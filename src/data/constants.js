b665dfb0af84b9091ef9a51cba82dc0e140e5b18
DurgaMounika666 <kasireddydurgamounika@gmail.com>
2024-09-06 22:20:27 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,276 @@
+
+export const Bio = {
+    name: "Durga Mounika Kasireddy",
+    roles: [
+      "Full Stack Developer",
+      "Data Analyst",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/DurgaMounika666",
+    linkedin: "https://www.linkedin.com/in/durga-mounika-kasireddy-b28854236/",
+    resume : "https://drive.google.com/file/d/1mL9K2vu0wLt9KtIgNFbahyPCQtuNl7CQ/view?usp=sharing",
+    insta: "https://www.instagram.com/mounika_kasireddy_666/?igsh=MXY1eHhrN20wcG50Mw%3D%3D",
+    
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Angular",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/f/f7/Angular_gradient.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "SQL",
+          image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sqldeveloper/sqldeveloper-original.svg",
+        },
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+
+        {
+          name: "Postgre SQL",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg",
+        },
+        
+      ],
+    },
+  
+    {
+      title: "Programming Languages",
+      skills: [
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        
+        {
+          name: "C Language",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+        },
+      ],
+    },
+    {
+      title: "Machine Learning",
+      skills: [
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "Jupyter",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
+        },
+      
+        {
+          name: "PyCharm",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/PyCharm_Icon.svg/512px-PyCharm_Icon.svg.png",
+        },
+      ],
+    },
+
+    {
+      title: "Microsoft Office",
+      skills: [
+        {
+          name: "MS Word",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/f/fd/Microsoft_Office_Word_%282019%E2%80%93present%29.svg",
+        },
+        {
+          name: "MS Excel",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/8/8d/Microsoft_Excel_Logo_%282013-2019%29.svg",
+        },
+      
+        {
+          name: "MS Powerpoint",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/0/0d/Microsoft_Office_PowerPoint_%282019%E2%80%93present%29.svg",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+      
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+        },
+        {
+          name: "PowerBI",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/c/cf/New_Power_BI_Logo.svg",
+        },
+        {
+          name: "Linux",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/3/34/Made_with_Linux_casper.svg",
+        },
+        {
+          name: "Git",
+          image:
+            "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+        },
+        {
+          name: "Windows",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/b/b8/Icon-windows_os.svg",
+        },
+        
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://upload.wikimedia.org/wikipedia/commons/0/09/Het_Grote_Bosgebeuren_logo..jpg", 
+      role: "Cyber Security Internship ",
+      company: "Blackbucks Engineer Private Limited ",
+      date: "Jul 2022 - Sep 2022 ",
+      desc: "During Internship, I have to Understand how to protect the sensitive information from unauthorized access or disclosure, ensuring data privacyand compliance with regulations.Measures to protect the integrity and usability of network and data, including firewalls, intrusion detection/preventionsystems (IDS/IPS), and secure access controls. Ensuring that software applications are secure against threats and vulnerabilities, often through secure coding practicesand application testing tools like Burp Suite and OWASP ZAP.",
+    },
+    
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://upload.wikimedia.org/wikipedia/commons/8/88/Education_icon_%28www.gov.ph%29.png",  
+      school: "Sagi Rama Krishnam Raju Engineering College, Bhimavaram",
+      date: "Aug 2020 - Jun 2024",
+      grade: "8.82 CGPA",
+      desc: "Recently completed my undergraduation of B.Tech in Artificiala Intelligence and Data Science at SRKR Engineering College, Bhimavaram with 8.82 CGPA. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.I am learning and working on exciting projects.",
+      degree: "Bachelor of Technology - BTech,  Specialization - Artificial Intelligence and Data Science",
+    },
+    {
+      id: 1,
+      img: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Military_Symbol_-_Friendly_Unit_%28Bichrome_1.5x1_Frame%29-_Military_Education_%28Hypothetical_APP-6A_Inspired%29.svg", 
+      school: "Pragati Junior College, Tanuku",
+      date: "Jun 2018 - Apr 2020",
+      grade: "9.88 CGPA",
+      desc: "I completed my class 12 high school education at Pragati Junior College, Tanuku, where I studied MPC.",
+      degree: "BIEAP (XII)",
+    },
+    {
+      id: 2,
+      img: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Military_Symbol_-_Friendly_Unit_%28Bichrome_1.5x1_Frame%29-_Military_Education_%28Hypothetical_APP-6A_Inspired%29.svg", 
+      school: "Indhira Gandhi Z.P.P.G.H.S, Attili",
+      date: "Jun 2017 - Apr 2018",
+      grade: "9.7 CGPA",
+      desc: "I completed my class 10 education at Indhira Gandhi Z.P.P.G.H.S, Attili",
+      degree: "BSEAP (X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Vulnerability Scanning and Bluetooth Hacking",
+      // date: "Sep 2022",
+      description:
+        "Developed and perform the Vulnerabilities Scanning using Scanning tools and Bluetooth Hacking technique with the help of BetterCap feature.Proactive security measure used to identify potential weaknesses in computer systems, networks, or applications that could be exploited by attackers.Utilize Scanning tools like Acunetix, Nmap and Metasploit for recognizing web application vulnerabilities.Technique used to get information from another Bluetooth enabled devices without any permissions from the host.",
+      image:
+        'https://i.imgur.com/Xp1VVLx.png' ,
+      tags: ["Kali Linux", "BetterCap", "Scanning Tools"],
+      category: "cyber security",
+    
+    },
+    {
+      id: 1,
+      title: "Fake Job Post Detection Using ML",
+      // date: "April 2024",
+      description:
+        "Designed and developed an innovative Fake Job Post detection automated tool using machine learning techniques to avoid the fraudulent job posts on the internet.  Scammers exploit by creating forged job ads to obtain personal information or demand payment from job seekers.Different classifiers like Random Forest, Decision Tree, SGD Classifier are used for checking fake post and the results of those classifiers are compared to identify SGD Classifier gives 91% of accuracy as the best employment detection model.",
+      image:
+        "https://i.imgur.com/xblgjKY.png",
+      tags: ["HTML", "CSS", "JavaScript", "Python", "Flask", "NumPy", "Microsoft Excel"],
+      category: "machine learning",
+      
+      
+    },
+    {
+      id: 2,
+      title: "Automatic Street Light System",
+      // date: "Mar 2022",
+      description:
+        "To design Automatic Street Light System using Embedded C, integrating microcontroller programming and sensors. Implemented an automated streetlight control system that reduced energy consumption by 40% annually; the system utilized sensors and AI, resulting in a significant decrease in operational costs and environmental impact. Intelligent lighting solution designed to enhance energy efficiency and operational convenience by automating the street lights based on ambient light conditions.",
+      image:
+        "https://i.imgur.com/40yBkna.png",
+      tags: ["Embedded C", "Arduino", "Sensors", "LED's"],
+      category: "iot",
+      // github: "https://github.com/divyansh709/RazorPay-Clone",
+      // webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+    },
+    
+    
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
+  
